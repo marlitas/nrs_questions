710b516ed72312220c326d86e3a07280d2c5d670
Marla Schulz <marlaa.schulz@gmail.com>
2022-01-13 17:59:16 -0700

    Analyze time complexity

--- foo/foo.cpp
+++ foo/foo.cpp
@@ -21,45 +21,36 @@ struct ThreeDPoint
 };
 
 
-
+//Depending on what we may expect inputs to be food could be done more efficiently with a merge sort approach. If the vector provided is sorted and short it could be a best case scenario of O(n), however if the vector provided is long or deeply scrambled foo has a time complexity of O(n^2)
+//I did not convert foo to a merge sort algorithm, so as to not change the entire function I was provided, but rather improve upon the algorithm given.
 void foo(std::vector<ThreeDPoint> points)
   {
     int i,j;
     //recommend changing to double for more precision when working with squareroots
-    //However double takes more memory, so argument can be made for float if memory usage is a limitation.
     double tmp1 = 0.0;
     double tmp2 = 0.0;
+    bool active = true;
 
-    //add one to i to eliminate error when i is last element in vector
-    for ( i = 0; i + 1 < points.size(); i++ )
+    while(active)
     {
-      j = i + 1;
-
-      //Finding squareroot of sum of squares, a slight modification of standard deviation
-      tmp1 = (float)sqrt( points[i].x * points[i].x + points[i].y * points[i].y + points[i].z * points[i].z );
-
-      tmp2 = (float)sqrt( points[j].x * points[j].x + points[j].y * points[j].y + points[j].z * points[j].z );
-
-      if ( tmp2 < tmp1 )
+      active = false;
+      //add one to i to eliminate error when i is last element in vector
+      for ( i = 0; i + 1 < points.size(); i++ )
       {
-        // std::cout << points[i].x;
-        // std::cout << points[i].y;
-        // std::cout << points[i].z;
+        j = i + 1;
 
-        // std::cout << points[j].x;
-        // std::cout << points[j].y;
-        // std::cout << points[j].z << std::endl;
+        //Finding squareroot of sum of squares, a slight modification of standard deviation
+        tmp1 = (float)sqrt( points[i].x * points[i].x + points[i].y * points[i].y + points[i].z * points[i].z );
 
-        //sort points based on modified standard deviation
-        std::swap(points[i], points[j]);
+        tmp2 = (float)sqrt( points[j].x * points[j].x + points[j].y * points[j].y + points[j].z * points[j].z );
 
-        // std::cout << points[i].x;
-        // std::cout << points[i].y;
-        // std::cout << points[i].z;
-
-        // std::cout << points[j].x;
-        // std::cout << points[j].y;
-        // std::cout << points[j].z << std::endl;
+        if ( tmp2 < tmp1 )
+        {
+          //sort points based on modified standard deviation
+          std::swap(points[i], points[j]);
+          //trigger while loop again to make sure all points are sorted
+          active = true;
+        }
       }
     }
   };
